b8b017c6ce9d9b632d7b66fcc0f04f9acd47df6c
Jan Pfeifer <pfeife@fim.uni-passau.de>
2024-01-17 14:02:08 +0100

    fixed code

--- puzzle.py
+++ puzzle.py
@@ -1,22 +1,16 @@
 import socket
 import hashlib
-import random
 
 # ISS connection
-server_ip = "net1-public.informatik.uni-hamburg.de"
+server_name = "net1-public.informatik.uni-hamburg.de"
 server_port = 23001
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     # Connect to the server
-    client_socket.connect((server_ip, server_port))
-    print(f"Connected to {server_ip}:{server_port}")
-
-    # Send data to the server
-    message = "I need a puzzle!"
-    client_socket.sendall(message.encode())
-    print(f"Sent: {message}")
+    client_socket.connect((server_name, server_port))
+    print(f"Connected to {server_name}:{server_port}")
 
     # Receive data from the server
     data = client_socket.recv(1024)
@@ -26,24 +20,20 @@ try:
     # Calculate matching hash
     def find_matching_hash():
         hashed_puzzle = hashlib.sha256(str(puzzle).encode('utf-8')).hexdigest()
+        candidate = int(puzzle) + 1
+
         while True:
-
-            candidate = random.Random().randint(0,1000000)
-            if candidate == puzzle:
-                continue
             hashed_candidate =  hashlib.sha256(str(candidate).encode('utf-8')).hexdigest()
-
             if hashed_candidate[:4] == hashed_puzzle[:4]:
-                print("Matching hash found!")
-                print(f"Hashed candidate: {hashed_candidate[:]}")
-                print(f"Hashed puzzle: {hashed_puzzle[:]}")
-
+                print(f"Hashed candidate: {hashed_candidate[:8]}")
+                print(f"Hashed puzzle: {hashed_puzzle[:8]}")
                 return candidate
+            candidate = candidate + 1
 
     # Send our result
     result = find_matching_hash()
-    client_socket.sendall(str(result).encode())
-    print(f"Sent: {str(result)}")
+    client_socket.send(str(result).encode("utf-8"))
+    print(f"Sent: {result}")
 
     # Recieve final answer from the server
     data = client_socket.recv(1024)
